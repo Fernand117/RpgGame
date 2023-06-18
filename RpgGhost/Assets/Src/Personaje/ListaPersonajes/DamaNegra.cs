29ad92a2ff2d2deb3a69bb69c8f27fdc258820a5
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2023-06-17 19:08:44 -0600

    Segundo escenario

---
+++ RpgGhost/Assets/Src/Personaje/ListaPersonajes/DamaNegra.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Src.Personaje.PersonajeBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Src.Personaje.ListaPersonajes
+{
+    public class DamaNegra : PersonajeBaseModel
+    {
+        public DamaNegra()
+        {
+            nombre = "La Dama Negra";
+        }
+
+        public override void Correr(NavMeshAgent navMeshAgent, Animator animator, AudioSource audioRun, Input input, float velocidadPj, bool isRun)
+        {
+        }
+    }
+}

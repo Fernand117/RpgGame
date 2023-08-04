4f25c79ea4f59ea7e4e30593cba52c7141cbe47f
Luis Fernando Leyva Luna <fernando_rendel892@hotmail.com>
2023-08-04 14:07:07 -0600

    aquí nos quedamos la última vez xD

--- RpgGhost/Assets/Src/Hechizos/ListaHechizos/HechizoFuego.cs
+++ RpgGhost/Assets/Src/Hechizos/ListaHechizos/HechizoFuego.cs
@@ -1,10 +1,4 @@
 ﻿using Assets.Src.Hechizos.HechizoBase;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEditor;
 using UnityEngine;
 
 namespace Assets.Src.Hechizos.ListaHechizos
